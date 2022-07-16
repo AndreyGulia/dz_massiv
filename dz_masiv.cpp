273192fd661c83769bc067491322e9bd3d09d798
AndriyGulia <smurfdota1337@yahoo.com>
2022-07-16 18:05:16 +0300

    First commit

---
+++ dz_masiv.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	const int arraySize{ 10 };
+	int arrayNumber[arraySize];
+
+	for (int i{}; i < arraySize; i++)
+	{
+		std::cout << "Enter" << i + 1 << " number of array :";
+		std::cin >> *(arrayNumber + i);
+	}
+
+	for (int i{}; i < arraySize; i++)
+	{
+		for (int j{ i + 1 }; j < arraySize; j++)
+		{
+			if (*(arrayNumber + j) < *(arrayNumber + i))
+			{
+				std::swap(*(arrayNumber + i), *(arrayNumber + j));
+			}
+		}
+	}
+
+
+	std::cout << "Sorted array: ";
+	for (int i{}; i < arraySize; i++)
+	{
+		std::cout << *(arrayNumber + i) << " ";
+	}
+	return 0;
+}
+  
